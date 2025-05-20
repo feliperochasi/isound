8804db5fc87712944dfd38318defba19cbf4c54e
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-05-20 10:42:33 -0300

    Le opcoes do menu e retorna funcao de acordo com a selecao do usuario

--- src/main/java/br/com/feliperochasi/isound/main/Main.java
+++ src/main/java/br/com/feliperochasi/isound/main/Main.java
@@ -18,6 +18,7 @@ public class Main {
         while (option != 9) {
             showMenu();
             option = scanner.nextInt();
+            readOption(option);
         }
     }
 
@@ -27,14 +28,83 @@ public class Main {
                 
                 1 - Cadastrar artista
                 2 - Cadastrar musica
-                3 - Listar musicas
-                4 - Buscar musica por artista
-                5 - Saber mais sobre artista
-                6 - Saber mais sobre musica
+                3 - Cadastrar Album
+                4 - Listar musicas
+                5 - Buscar musica por artista
+                6 - Buscar musica por album
+                7 - Saber mais sobre artista
+                8 - Saber mais sobre musica
                 
                 9 - Sair
                 """);
     }
 
+    private void readOption(Integer selectOption) {
+        switch (selectOption) {
+            case 1:
+                createNewArtisct();
+                break;
+            case 2:
+                createNewMusic();
+                break;
+            case 3:
+                createNewAlbum();
+                break;
+            case 4:
+                listAllMusics();
+                break;
+            case 5:
+                searchMusicFromArtisct();
+                break;
+            case 6:
+                searchMusicFromAlbum();
+                break;
+            case 7:
+                queryArtisctIA();
+                break;
+            case 8:
+                queryMusicIA();
+                break;
+            case 9:
+                System.out.println("Encerrando....");
+                break;
+            default:
+                System.out.println("Opcao invalida, tente novamente!");
+                break;
+        }
+
+
+    }
+
+    private void createNewArtisct() {
+
+    }
+
+    private void createNewMusic() {
+
+    }
+
+    private void createNewAlbum() {
+
+    }
+
+    private void listAllMusics() {
+
+    }
+
+    private void searchMusicFromArtisct() {
+
+    }
+
+    private void searchMusicFromAlbum() {
+
+    }
+
+    private void queryArtisctIA() {
+
+    }
+
+    private void queryMusicIA() {
+    }
 
 }
