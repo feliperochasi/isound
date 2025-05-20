31ca3f217a7670f205945cc8fd2341721a3c81fd
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-05-20 10:20:08 -0300

    Start project

---
+++ src/main/java/br/com/feliperochasi/isound/IsoundApplication.java
@@ -0,0 +1,13 @@
+package br.com.feliperochasi.isound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsoundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/feliperochasi/isound/IsoundApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.feliperochasi.isound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
